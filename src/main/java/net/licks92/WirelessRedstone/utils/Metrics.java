bd6898f0542a3e273eb2e93bec8981584fc299fc
Licks92 <licks92@gmail.com>
2012-04-16 14:18:06 +0200

    Updated metrics to mcstats.org Signed-off-by: Licks92 <licks92@gmail.com>

--- src/main/java/net/licks92/WirelessRedstone/utils/Metrics.java
+++ src/main/java/net/licks92/WirelessRedstone/utils/Metrics.java
@@ -75,7 +75,7 @@ public class Metrics {
     /**
      * The base url of the metrics domain
      */
-    private static final String BASE_URL = "http://metrics.griefcraft.com";
+    private static final String BASE_URL = "http://mcstats.org";
 
     /**
      * The url used to report a server's status
@@ -155,7 +155,7 @@ public class Metrics {
 
         // Do we need to create the file?
         if (configuration.get("guid", null) == null) {
-            configuration.options().header("http://metrics.griefcraft.com").copyDefaults(true);
+            configuration.options().header("http://mcstats.org").copyDefaults(true);
             configuration.save(configurationFile);
         }
 
