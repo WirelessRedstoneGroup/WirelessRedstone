f9dfe4bcb097256fb71e6466b160c9fb8ad5434b
Licks92 <licks92@gmail.com>
2012-08-18 08:46:04 -0700

    Updated metrics

--- src/main/java/net/licks92/WirelessRedstone/Utils/Metrics.java
+++ src/main/java/net/licks92/WirelessRedstone/Utils/Metrics.java
@@ -28,6 +28,8 @@ package net.licks92.WirelessRedstone.Utils;
  * either expressed or implied, of anybody else.
  */
 
+package org.mcstats;
+
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.configuration.InvalidConfigurationException;
@@ -81,11 +83,6 @@ public class Metrics {
      * The url used to report a server's status
      */
     private static final String REPORT_URL = "/report/%s";
-
-    /**
-     * The file where guid and opt out is stored in
-     */
-    private static final String CONFIG_FILE = "plugins/PluginMetrics/config.yml";
 
     /**
      * The separator to use for custom data. This MUST NOT change unless you are hosting your own
@@ -146,7 +143,7 @@ public class Metrics {
         this.plugin = plugin;
 
         // load the config
-        configurationFile = new File(CONFIG_FILE);
+        configurationFile = getConfigFile();
         configuration = YamlConfiguration.loadConfiguration(configurationFile);
 
         // add some defaults
@@ -167,7 +164,7 @@ public class Metrics {
      * Construct and create a Graph that can be used to separate specific plotters to their own graphs
      * on the metrics website. Plotters can be added to the graph object returned.
      *
-     * @param name
+     * @param name The name of the graph
      * @return Graph object created. Will never return NULL under normal circumstances unless bad parameters are given
      */
     public Graph createGraph(final String name) {
@@ -186,9 +183,22 @@ public class Metrics {
     }
 
     /**
+     * Add a Graph object to Metrics that represents data for the plugin that should be sent to the backend
+     *
+     * @param graph The name of the graph
+     */
+    public void addGraph(final Graph graph) {
+        if (graph == null) {
+            throw new IllegalArgumentException("Graph cannot be null");
+        }
+
+        graphs.add(graph);
+    }
+
+    /**
      * Adds a custom data plotter to the default graph
      *
-     * @param plotter
+     * @param plotter The plotter to use to plot custom data
      */
     public void addCustomData(final Plotter plotter) {
         if (plotter == null) {
@@ -234,6 +244,10 @@ public class Metrics {
                             if (isOptOut() && taskId > 0) {
                                 plugin.getServer().getScheduler().cancelTask(taskId);
                                 taskId = -1;
+                                // Tell all plotters to stop gathering information.
+                                for (Graph graph : graphs){
+                                    graph.onOptOut();
+                                }
                             }
                         }
 
@@ -258,13 +272,13 @@ public class Metrics {
     /**
      * Has the server owner denied plugin metrics?
      *
-     * @return
+     * @return true if metrics should be opted out of it
      */
     public boolean isOptOut() {
         synchronized(optOutLock) {
             try {
                 // Reload the metrics file
-                configuration.load(CONFIG_FILE);
+                configuration.load(getConfigFile());
             } catch (IOException ex) {
                 Bukkit.getLogger().log(Level.INFO, "[Metrics] " + ex.getMessage());
                 return true;
@@ -320,6 +334,23 @@ public class Metrics {
     }
 
     /**
+     * Gets the File object of the config file that should be used to store data such as the GUID and opt-out status
+     *
+     * @return the File object for the config file
+     */
+    public File getConfigFile() {
+        // I believe the easiest way to get the base folder (e.g craftbukkit set via -P) for plugins to use
+        // is to abuse the plugin object we already have
+        // plugin.getDataFolder() => base/plugins/PluginA/
+        // pluginsFolder => base/plugins/
+        // The base is not necessarily relative to the startup directory.
+        File pluginsFolder = plugin.getDataFolder().getParentFile();
+
+        // return => base/plugins/PluginMetrics/config.yml
+        return new File(new File(pluginsFolder, "PluginMetrics"), "config.yml");
+    }
+
+    /**
      * Generic method that posts a plugin to the metrics website
      */
     private void postPlugin(final boolean isPing) throws IOException {
@@ -347,10 +378,6 @@ public class Metrics {
             while (iter.hasNext()) {
                 final Graph graph = iter.next();
 
-                // Because we have a lock on the graphs set already, it is reasonable to assume
-                // that our lock transcends down to the individual plotters in the graphs also.
-                // Because our methods are private, no one but us can reasonably access this list
-                // without reflection so this is a safe assumption without adding more code.
                 for (Plotter plotter : graph.getPlotters()) {
                     // The key name to send to the metrics server
                     // The format is C-GRAPHNAME-PLOTTERNAME where separator - is defined at the top
@@ -414,13 +441,12 @@ public class Metrics {
                 }
             }
         }
-        //if (response.startsWith("OK")) - We should get "OK" followed by an optional description if everything goes right
     }
 
     /**
      * Check if mineshafter is present. If it is, we need to bypass it to send POST requests
      *
-     * @return
+     * @return true if mineshafter is installed on the server
      */
     private boolean isMineshafterPresent() {
         try {
@@ -440,10 +466,9 @@ public class Metrics {
      * encodeDataPair(data, "version", description.getVersion());
      * </code>
      *
-     * @param buffer
-     * @param key
-     * @param value
-     * @return
+     * @param buffer the stringbuilder to append the data pair onto
+     * @param key the key value
+     * @param value the value
      */
     private static void encodeDataPair(final StringBuilder buffer, final String key, final String value) throws UnsupportedEncodingException {
         buffer.append('&').append(encode(key)).append('=').append(encode(value));
@@ -452,8 +477,8 @@ public class Metrics {
     /**
      * Encode text as UTF-8
      *
-     * @param text
-     * @return
+     * @param text the text to encode
+     * @return the encoded text, as UTF-8
      */
     private static String encode(final String text) throws UnsupportedEncodingException {
         return URLEncoder.encode(text, "UTF-8");
@@ -482,7 +507,7 @@ public class Metrics {
         /**
          * Gets the graph's name
          *
-         * @return
+         * @return the Graph's name
          */
         public String getName() {
             return name;
@@ -491,7 +516,7 @@ public class Metrics {
         /**
          * Add a plotter to the graph, which will be used to plot entries
          *
-         * @param plotter
+         * @param plotter the plotter to add to the graph
          */
         public void addPlotter(final Plotter plotter) {
             plotters.add(plotter);
@@ -500,7 +525,7 @@ public class Metrics {
         /**
          * Remove a plotter from the graph
          *
-         * @param plotter
+         * @param plotter the plotter to remove from the graph
          */
         public void removePlotter(final Plotter plotter) {
             plotters.remove(plotter);
@@ -509,7 +534,7 @@ public class Metrics {
         /**
          * Gets an <b>unmodifiable</b> set of the plotter objects in the graph
          *
-         * @return
+         * @return an unmodifiable {@link Set} of the plotter objects
          */
         public Set<Plotter> getPlotters() {
             return Collections.unmodifiableSet(plotters);
@@ -528,6 +553,12 @@ public class Metrics {
 
             final Graph graph = (Graph) object;
             return graph.name.equals(name);
+        }
+
+        /**
+         * Called when the server owner decides to opt-out of Metrics while the server is running.
+         */
+        protected void onOptOut() {
         }
 
     }
@@ -552,16 +583,19 @@ public class Metrics {
         /**
          * Construct a plotter with a specific plot name
          *
-         * @param name
+         * @param name the name of the plotter to use, which will show up on the website
          */
         public Plotter(final String name) {
             this.name = name;
         }
 
         /**
-         * Get the current value for the plotted point
+         * Get the current value for the plotted point. Since this function defers to an external function
+         * it may or may not return immediately thus cannot be guaranteed to be thread friendly or safe.
+         * This function can be called from any thread so care should be taken when accessing resources
+         * that need to be synchronized.
          *
-         * @return
+         * @return the current value for the point to be plotted.
          */
         public abstract int getValue();
 
@@ -582,7 +616,7 @@ public class Metrics {
 
         @Override
         public int hashCode() {
-            return getColumnName().hashCode() + getValue();
+            return getColumnName().hashCode();
         }
 
         @Override
@@ -594,7 +628,5 @@ public class Metrics {
             final Plotter plotter = (Plotter) object;
             return plotter.name.equals(name) && plotter.getValue() == getValue();
         }
-
     }
-
 }
