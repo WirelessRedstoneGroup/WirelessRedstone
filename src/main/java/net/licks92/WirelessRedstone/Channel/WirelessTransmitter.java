77022bd1407c96545026493a280e7e34b040125f
Licks <licks92@gmail.com>
2013-03-29 23:39:58 +0100

    Implemented method isActive for receiver

--- src/main/java/net/licks92/WirelessRedstone/Channel/WirelessTransmitter.java
+++ src/main/java/net/licks92/WirelessRedstone/Channel/WirelessTransmitter.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.configuration.serialization.SerializableAs;
 
@@ -37,7 +40,12 @@ public class WirelessTransmitter implements ConfigurationSerializable, IWireless
 	
 	public boolean isActive()
 	{
-		return false;
+		Location loc = new Location(Bukkit.getWorld(getWorld()), getX(), getY(), getZ());
+		Block block = loc.getBlock();
+		if(block.isBlockIndirectlyPowered() || block.isBlockPowered())
+			return true;
+		else
+			return false;
 	}
 
 	@Override
