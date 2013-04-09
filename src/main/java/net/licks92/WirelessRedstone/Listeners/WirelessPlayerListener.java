313e0657c22de76f577a950a0b696d75642be111
Licks <licks92@gmail.com>
2013-04-09 22:46:53 +0200

    Add new PlayerInteractEvent
    If the player interacts with a transmitter, it will simply turn on a
    channel.

--- src/main/java/net/licks92/WirelessRedstone/Listeners/WirelessPlayerListener.java
+++ src/main/java/net/licks92/WirelessRedstone/Listeners/WirelessPlayerListener.java
@@ -1,9 +1,12 @@
 package net.licks92.WirelessRedstone.Listeners;
 
 import net.licks92.WirelessRedstone.WirelessRedstone;
+import net.licks92.WirelessRedstone.Channel.WirelessChannel;
 
+import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class WirelessPlayerListener implements Listener
@@ -39,5 +42,27 @@ public class WirelessPlayerListener implements Listener
 				ex.printStackTrace();
 			}
 		}
+	}
+	
+	/**
+	 * Used for handling right click on a transmitter.
+	 * If  a player interacts with a transmitter, it will turn on the channel for a given time.
+	 * 
+	 * @param event
+	 */
+	@EventHandler
+	public void onPlayerRightClick(PlayerInteractEvent event)
+	{
+		if(!(event.getClickedBlock().getState() instanceof Sign))
+			return;
+		
+		Sign sign = (Sign)event.getClickedBlock().getState();
+		
+		if(!WirelessRedstone.WireBox.isTransmitter(sign.getLine(0)) || sign.getLine(1) == null || sign.getLine(1) == "")
+			return;
+		
+		WirelessChannel channel = WirelessRedstone.config.getWirelessChannel(sign.getLine(1));
+		
+		channel.turnOn();
 	}
 }
