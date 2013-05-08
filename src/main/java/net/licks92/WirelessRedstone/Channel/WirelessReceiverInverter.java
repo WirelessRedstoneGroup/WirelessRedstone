e85d712eba0813860e4c4327448ac2d41998853a
Licks <licks92@gmail.com>
2013-05-08 17:49:06 +0200

    Fixed 3rd line of the inverter not coming back with the tag [inverter] when turning on the sign.

--- src/main/java/net/licks92/WirelessRedstone/Channel/WirelessReceiverInverter.java
+++ src/main/java/net/licks92/WirelessRedstone/Channel/WirelessReceiverInverter.java
@@ -2,6 +2,9 @@ package net.licks92.WirelessRedstone.Channel;
 
 import java.util.Map;
 
+import net.licks92.WirelessRedstone.WirelessRedstone;
+
+import org.bukkit.block.Sign;
 import org.bukkit.configuration.serialization.SerializableAs;
 
 @SerializableAs("WirelessReceiverInverter")
@@ -23,6 +26,9 @@ public class WirelessReceiverInverter extends WirelessReceiver implements IWirel
 	public void turnOn(String channelName)
 	{
 		super.turnOff(channelName);
+		Sign sign = (Sign) getLocation().getBlock().getState();
+		sign.setLine(2, WirelessRedstone.strings.tagsReceiverInverterType.get(0));
+		sign.update();
 	}
 	
 	@Override
