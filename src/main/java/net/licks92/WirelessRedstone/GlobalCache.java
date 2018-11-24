037beab4a42e0cd106d4098ae61448e83f76e0e6
Bart-0110 <Bart-0110@users.noreply.github.com>
2018-11-24 14:19:42 +0100

    Update GlobalCache.java

--- src/main/java/net/licks92/WirelessRedstone/GlobalCache.java
+++ src/main/java/net/licks92/WirelessRedstone/GlobalCache.java
@@ -80,7 +80,7 @@ public class GlobalCache {
     }
 
     public void update() {
-        update(ConfigManager.getConfig().getStorageType() == StorageType.YAML); //Changed to sync as temp solution to avoid database locked exceptions
+        update(false); //Changed to sync as temp solution to avoid database locked exceptions
     }
 
     public void update(boolean async) {
